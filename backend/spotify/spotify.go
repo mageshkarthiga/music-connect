fab4a8935acf69704f0a745229ca198923fa7a3e
mageshkarthiga <mageshk.2021@scis.smu.edu.sg>
2025-03-18 20:50:56 +0800

    feat: spotify web playback API test

--- backend/spotify/spotify.go
+++ backend/spotify/spotify.go
@@ -1,49 +1,58 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"os"
-	"github.com/joho/godotenv"
-	"github.com/zmb3/spotify"
-	"golang.org/x/oauth2/clientcredentials"
+    "context"
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "os"
+
+    "github.com/joho/godotenv"
+    "github.com/zmb3/spotify"
+    "golang.org/x/oauth2/clientcredentials"
+)
+
+var (
+    authConfig *clientcredentials.Config
 )
 
 func main() {
-	err := godotenv.Load("../.env")
-	if err != nil {
-		fmt.Println("Error loading .env file")
-		return
-	}
-
-	clientId := os.Getenv("SPOTIFY_CLIENT_ID")
-	clientSecret := os.Getenv("SPOTIFY_CLIENT_SECRET")
-	if clientId == "" || clientSecret == "" {
-		fmt.Println("Spotify client id or client secret not found")
-		return
-	}
-	// to authenticate and get token
-	authConfig := &clientcredentials.Config{
-		ClientID:     clientId,
-		ClientSecret: clientSecret,
-		TokenURL:     spotify.TokenURL,
-	}
-	accessToken, err := authConfig.Token(context.Background())
-	if err != nil {
-		fmt.Println("error retrieving access token: %v", err)
-	}
-
-	// connect to Spotify API
-	spotifyClient := spotify.Authenticator{}.NewClient(accessToken)
-	artistID := spotify.ID("4zCH9qm4R2DADamUHMCa6O")
-	artist, err := spotifyClient.GetArtist(artistID)
-
-	if err != nil {
-        fmt.Printf("error retrieving artist: %v\n", err)
+    err := godotenv.Load("../.env")
+    if err != nil {
+        fmt.Println("Error loading .env file")
         return
     }
 
+    clientId := os.Getenv("SPOTIFY_CLIENT_ID")
+    clientSecret := os.Getenv("SPOTIFY_CLIENT_SECRET")
+    if clientId == "" || clientSecret == "" {
+        fmt.Println("Spotify client id or client secret not found")
+        return
+    }
 
-	fmt.Println("artist id:", artist.ID)
-	fmt.Println("artist name:", artist.Name)
+    authConfig = &clientcredentials.Config{
+        ClientID:     clientId,
+        ClientSecret: clientSecret,
+        TokenURL:     spotify.TokenURL,
+        Scopes:       []string{"user-read-playback-state", "user-modify-playback-state", "streaming"},
+    }
+
+    http.HandleFunc("/token", getTokenHandler)
+    fmt.Println("Server is running on port 8080")
+    http.ListenAndServe(":8080", nil)
+}
+
+func getTokenHandler(w http.ResponseWriter, r *http.Request) {
+    enableCors(&w)
+    token, err := authConfig.Token(context.Background())
+    if err != nil {
+        http.Error(w, "Failed to get token", http.StatusInternalServerError)
+        return
+    }
+
+    json.NewEncoder(w).Encode(token)
+}
+
+func enableCors(w *http.ResponseWriter) {
+    (*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
