f8932d36fe06f538803a4d733e661a6f12cfe7b1
mageshkarthiga <mageshk.2021@scis.smu.edu.sg>
2025-03-18 16:41:56 +0800

    fix: modify key name for many to many tables

--- music-connect/api/config.go
+++ music-connect/api/config.go
@@ -17,19 +17,19 @@ type User struct {
 	EmailAddress string
 	Location     string
 	UserName     string
-	Events       []*Event `gorm:"many2many:user_events"`
-	Tracks       []*Track `gorm:"many2many:music_preferences"`
+	Events       []*Event `gorm:"many2many:user_events;joinForeignKey:UserID;joinReferences:EventID"`
+	Tracks       []*Track `gorm:"many2many:music_preferences;joinForeignKey:UserID;joinReferences:TrackID"`
 }
 
 type Event struct {
-	EventID          uint `gorm:"primaryKey;autoIncrement"`
+	EventID          uint   `gorm:"primaryKey;autoIncrement"`
 	EventName        string
 	EventDescription string
 	EventURL         string
 	EventImageURL    string
-	Users            []*User       `gorm:"many2many:user_events"`
-	Attractions      []*Attraction `gorm:"many2many:event_attractions"`
-	Venue            []*Venue      `gorm:"many2many:user_venues"`
+	Users            []*User       `gorm:"many2many:user_events;joinForeignKey:EventID;joinReferences:UserID"`
+	Attractions      []*Attraction `gorm:"many2many:event_attractions;joinForeignKey:EventID;joinReferences:AttractionID"`
+	Venues           []*Venue      `gorm:"many2many:event_venues;joinForeignKey:EventID;joinReferences:VenueID"`
 }
 
 type Attraction struct {
@@ -38,7 +38,7 @@ type Attraction struct {
 	AttractionType     string
 	AttractionURL      string
 	AttractionImageURL string
-	Events             []*Event `gorm:"many2many:event_attractions"`
+	Events             []*Event `gorm:"many2many:event_attractions;joinForeignKey:AttractionID;joinReferences:EventID"`
 }
 
 type Venue struct {
@@ -47,14 +47,14 @@ type Venue struct {
 	Location      string
 	SeatMap       string
 	Accessibility string
-	Events        []*Event `gorm:"many2many:event_venues"`
+	Events        []*Event `gorm:"many2many:event_venues;joinForeignKey:VenueID;joinReferences:EventID"`
 }
 
 type Artist struct {
 	ArtistID       uint `gorm:"primaryKey;autoIncrement"`
 	ArtistName     string
 	ArtistImageURL string
-	Tracks         []*Track `gorm:"many2many:track_artists"`
+	Tracks         []*Track `gorm:"many2many:track_artists;joinForeignKey:ArtistID;joinReferences:TrackID"`
 }
 
 type Track struct {
@@ -63,15 +63,16 @@ type Track struct {
 	ArtistID   uint `gorm:"foreignKey:artist_id"`
 	Genre      string
 	TrackURL   string
-	Artists    []*Artist   `gorm:"many2many:track_artists"`
-	Playlists  []*Playlist `gorm:"many2many:playlist_tracks"`
+	Artists    []*Artist   `gorm:"many2many:track_artists;joinForeignKey:TrackID;joinReferences:ArtistID"`
+	Playlists  []*Playlist `gorm:"many2many:playlist_tracks;joinForeignKey:TrackID;joinReferences:PlaylistID"`
 }
 
 type Playlist struct {
 	PlaylistID   uint `gorm:"primaryKey;autoIncrement"`
 	PlaylistName string
-	Tracks       []*Track `gorm:"many2many:playlist_tracks"`
+	Tracks       []*Track `gorm:"many2many:playlist_tracks;joinForeignKey:PlaylistID;joinReferences:TrackID"`
 }
+
 
 // definition of table structure ends here
 
@@ -99,6 +100,16 @@ func main() {
 	fmt.Println("Successfully connected to the database! ✅", db)
 
 	// Check if tables exist and drop if they do
+	manyToManyTables := []string{
+		"user_events", "music_preferences", "event_attractions",
+		"event_venues", "track_artists", "playlist_tracks",
+	}
+	for _, tableName := range manyToManyTables {
+		if db.Migrator().HasTable(tableName) {
+			db.Migrator().DropTable(tableName)
+		}
+	}
+
 	tables := []interface{}{&User{}, &Event{}, &Attraction{}, &Venue{}, &Artist{}, &Track{}, &Playlist{}}
 	for _, table := range tables {
 		if db.Migrator().HasTable(table) {
