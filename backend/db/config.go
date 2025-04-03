0d3c78b1b7ee5eb147aaca0f95d6f6a8ec2a7de3
mageshkarthiga <mageshk.2021@scis.smu.edu.sg>
2025-04-03 20:17:04 +0800

    Revert "fix: prepared statement error fix"
    This reverts commit 00a466847153ad0d66a024daa8b58e37219b0005.

--- backend/db/config.go
+++ backend/db/config.go
@@ -52,10 +52,10 @@ type Venue struct {
 }
 
 type Artist struct {
-	ArtistID        uint `gorm:"primaryKey;autoIncrement"`
-	ArtistName      string
-	ArtistSpotifyID string
-	Tracks          []*Track `gorm:"many2many:track_artists;joinForeignKey:ArtistID;joinReferences:TrackID"`
+	ArtistID       uint `gorm:"primaryKey;autoIncrement"`
+	ArtistName     string
+	ArtistImageURL string
+	Tracks         []*Track `gorm:"many2many:track_artists;joinForeignKey:ArtistID;joinReferences:TrackID"`
 }
 
 type Track struct {
@@ -63,7 +63,7 @@ type Track struct {
 	TrackTitle string
 	ArtistID   uint `gorm:"foreignKey:artist_id"`
 	Genre      string
-	TrackURI   string
+	TrackURL   string
 	Artists    []*Artist   `gorm:"many2many:track_artists;joinForeignKey:TrackID;joinReferences:ArtistID"`
 	Playlists  []*Playlist `gorm:"many2many:playlist_tracks;joinForeignKey:TrackID;joinReferences:PlaylistID"`
 }
@@ -78,7 +78,7 @@ type Device struct {
 	DeviceID   uint `gorm:"primaryKey;autoIncrement"`
 	DeviceName string
 	DeviceUUID string `gorm:"uniqueIndex"`
-	UserID     uint
+	UserID     uint 
 }
 
 // definition of table structure ends here
@@ -98,8 +98,7 @@ func main() {
 
 	dsn := fmt.Sprintf("postgresql://postgres.kzxuobrnlppliqiwwgvu:%s@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres", dbPassword)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		PrepareStmt: true,
-		Logger:      logger.Default.LogMode(logger.Info),
+		Logger: logger.Default.LogMode(logger.Info),
 	}) // open DB connection to supabase postgres via gorm
 	if err != nil {
 		fmt.Println("Error connecting to database ⚠️", err)
@@ -118,126 +117,18 @@ func main() {
 		}
 	}
 
-	tables := []interface{}{
-		&Device{}, &Playlist{}, &Track{}, &Artist{},
-		&Venue{}, &Attraction{}, &Event{}, &User{},
-	}
+	tables := []interface{}{&User{}, &Event{}, &Attraction{}, &Venue{}, &Artist{}, &Track{}, &Playlist{}, &Device{}}
 	for _, table := range tables {
 		if db.Migrator().HasTable(table) {
-			err := db.Migrator().DropTable(table)
-			if err != nil {
-				fmt.Printf("Error dropping table %T: %v\n", table, err)
-				return
-			}
+			db.Migrator().DropTable(table)
 		}
 	}
+
 	// Commit tables to DB
 	err = db.AutoMigrate(&User{}, &Event{}, &Attraction{}, &Venue{}, &Artist{}, &Track{}, &Playlist{}, &Device{})
-	session := db.Session(&gorm.Session{PrepareStmt: true})
-	if session != nil {
-		fmt.Println("Migration successful")
-	}
-
-	fmt.Println("Migration completed successfully!")
-
-	// Add dummy data
-	artist1 := Artist{
-		ArtistName:      "ROSE",
-		ArtistSpotifyID: "3eVa5w3URK5duf6eyVDbu9",
-	}
-
-	artist2 := Artist{
-		ArtistName:      "Kendrick Lamar",
-		ArtistSpotifyID: "2YZyLoL8N0Wb9xBt1NhZWg",
-	}
-
-	artist3 := Artist{
-		ArtistName:      "LE SSERAFIM",
-		ArtistSpotifyID: "4SpbR6yFEvexJuaBpgAU5p",
-	}
-
-	artist4 := Artist{
-		ArtistName:      "Selena Gomez",
-		ArtistSpotifyID: "0C8ZW7ezQVs4URX5aX7Kqx",
-	}
-
-	artist5 := Artist{
-		ArtistName:      "Bruno Mars",
-		ArtistSpotifyID: "0du5cEVh5yTK9QJze8zA0C",
-	}
-
-	artist6 := Artist{
-		ArtistName:      "Sabrina Carpenter",
-		ArtistSpotifyID: "74KM79TiuVKeVCqs8QtB0B",
-	}
-
-	artist7 := Artist{
-		ArtistName:      "Doechii",
-		ArtistSpotifyID: "4E2rKHVDssGJm2SCDOMMJB",
-	}
-
-	artist8 := Artist{
-		ArtistName:      "Benson Boone",
-		ArtistSpotifyID: "22wbnEMDvgVIAGdFeek6ET",
-	}
-	artist9 := Artist{
-		ArtistName:      "Adrianna Cinta",
-		ArtistSpotifyID: "7aGKWIJ44Gs7eQ7cCKVskG",
-	}
-	artist10 := Artist{
-		ArtistName:      "James Arthur",
-		ArtistSpotifyID: "4IWBUUAFIplrNtaOHcJPRM",
-	}
-
-	//Insert artists into the database
-	err = db.Create(&artist1).Error
 	if err != nil {
-		fmt.Println("Error inserting artist1:", err)
+		fmt.Println("Error during migration ⚠️", err)
 		return
 	}
-	err = db.Create(&artist2).Error
-	if err != nil {
-		fmt.Println("Error inserting artist2:", err)
-		return
-	}
-	err = db.Create(&artist3).Error
-	if err != nil {
-		fmt.Println("Error inserting artist3:", err)
-		return
-	}
-	err = db.Create(&artist4).Error
-	if err != nil {
-		fmt.Println("Error inserting artist4:", err)
-		return
-	}
-	err = db.Create(&artist5).Error
-	if err != nil {
-		fmt.Println("Error inserting artist5:", err)
-		return
-	}
-	err = db.Create(&artist6).Error
-	if err != nil {
-		fmt.Println("Error inserting artist6:", err)
-		return
-	}
-	err = db.Create(&artist7).Error
-	if err != nil {
-		fmt.Println("Error inserting artist7:", err)
-		return
-	}
-	err = db.Create(&artist8).Error
-	if err != nil {
-		fmt.Println("Error inserting artist8:", err)
-		return
-	}
-	err = db.Create(&artist9).Error
-	if err != nil {
-		fmt.Println("Error inserting artist9:", err)
-		return
-	}
-	err = db.Create(&artist10).Error
-	if err != nil {
-		fmt.Println("Error inserting artist10:", err)
-		return
-	}
+
 }
