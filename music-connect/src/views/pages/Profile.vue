8a421f3e67c74117408b3d7585d3f4206e309ce3
Deric <deric.yeap1@gmail.com>
2025-04-03 21:05:10 +0800

    google sign in

--- music-connect/src/views/pages/Profile.vue
+++ music-connect/src/views/pages/Profile.vue
@@ -53,7 +53,7 @@ export default defineComponent({
   <!-- Inline styling can be awkward, but it's requested here. -->
   <div
     class="profile-page"
-    style="max-width: 500px; margin: 2rem auto; text-align: center"
+    style="max-width: 1000px; margin: 2rem auto; text-align: center"
   >
     <div
       v-if="loading"
@@ -83,5 +83,73 @@ export default defineComponent({
     <div v-else class="error p-error" style="font-size: 1.2rem; padding: 2rem">
       <p>User not found.</p>
     </div>
+
+    <Fluid>
+      <div class="flex gap-4 mb-4 md:flex">
+        <div class="md:w-1/2">
+          <div class="card flex flex-col w-full">
+            <div class="font-semibold text-xl mb-4">Skeleton</div>
+            <div class="rounded-border border border-surface p-6">
+              <div class="flex mb-4">
+                <Skeleton shape="circle" size="4rem" class="mr-2"></Skeleton>
+                <div>
+                  <Skeleton width="10rem" class="mb-2"></Skeleton>
+                  <Skeleton width="5rem" class="mb-2"></Skeleton>
+                  <Skeleton height=".5rem"></Skeleton>
+                </div>
+              </div>
+              <Skeleton width="100%" height="150px"></Skeleton>
+              <div class="flex justify-between mt-4">
+                <Skeleton width="4rem" height="2rem"></Skeleton>
+                <Skeleton width="4rem" height="2rem"></Skeleton>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="md:w-1/2">
+          <div class="card flex flex-col w-full">
+            <div class="font-semibold text-xl mb-4">Skeleton</div>
+            <div class="rounded-border border border-surface p-6">
+              <div class="flex mb-4">
+                <Skeleton shape="circle" size="4rem" class="mr-2"></Skeleton>
+                <div>
+                  <Skeleton width="10rem" class="mb-2"></Skeleton>
+                  <Skeleton width="5rem" class="mb-2"></Skeleton>
+                  <Skeleton height=".5rem"></Skeleton>
+                </div>
+              </div>
+              <Skeleton width="100%" height="150px"></Skeleton>
+              <div class="flex justify-between mt-4">
+                <Skeleton width="4rem" height="2rem"></Skeleton>
+                <Skeleton width="4rem" height="2rem"></Skeleton>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex gap-4 md:flex">
+        <div class="md:w-1/2">
+          <div class="card flex flex-col w-full">
+            <div class="font-semibold text-xl mb-4">Skeleton</div>
+            <div class="rounded-border border border-surface p-6">
+              <div class="flex mb-4">
+                <Skeleton shape="circle" size="4rem" class="mr-2"></Skeleton>
+                <div>
+                  <Skeleton width="10rem" class="mb-2"></Skeleton>
+                  <Skeleton width="5rem" class="mb-2"></Skeleton>
+                  <Skeleton height=".5rem"></Skeleton>
+                </div>
+              </div>
+              <Skeleton width="100%" height="150px"></Skeleton>
+              <div class="flex justify-between mt-4">
+                <Skeleton width="4rem" height="2rem"></Skeleton>
+                <Skeleton width="4rem" height="2rem"></Skeleton>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fluid>
   </div>
 </template>
