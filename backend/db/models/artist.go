1f87ab9e7943cef5f15411c465c8cd46910b2efe
Deric <deric.yeap1@gmail.com>
2025-04-05 15:07:47 +0800

    delete old db folder

--- backend/db/models/artist.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Artist struct {
-	ArtistID       uint `gorm:"primaryKey;autoIncrement"`
-	ArtistName     string
-	ArtistImageURL string
-	Tracks         []*Track `gorm:"many2many:track_artists;joinForeignKey:ArtistID;joinReferences:TrackID"`
-}
--- backend/db/models/playlist.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Playlist struct {
-	PlaylistID   uint `gorm:"primaryKey;autoIncrement"`
-	PlaylistName string
-	Tracks       []*Track `gorm:"many2many:playlist_tracks;joinForeignKey:PlaylistID;joinReferences:TrackID"`
-}
