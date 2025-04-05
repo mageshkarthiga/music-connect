e69a31244951346446c96b41f7f35e20094ed619
mageshkarthiga <mageshk.2021@scis.smu.edu.sg>
2025-04-05 22:48:32 +0800

    feat: new gcp link

--- frontend/src/components/SpotifyPlayer.vue
+++ frontend/src/components/SpotifyPlayer.vue
@@ -49,7 +49,7 @@ export default {
         };
     },
     async mounted() {
-        const response = await axios.get("http://localhost:8080/spotify/token");
+        const response = await axios.get("https://music-connect-226566573804.us-central1.run.app/spotify/token");
         this.accessToken = response.data.access_token;
         this.loadSpotifyContent("spotify:track:5r7egnfTIQjaKSGREhIky9");
 
