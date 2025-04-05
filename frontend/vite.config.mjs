be7f1014a9805aed2eb5e57fd4f1074cd4d6ea56
Deric <deric.yeap1@gmail.com>
2025-04-05 15:38:50 +0800

    Update vite.config.mjs

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, loadEnv } from "vite";
 import path from "path";
 import dotenv from "dotenv";
 
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, path.resolve(__dirname, ".."), "");
