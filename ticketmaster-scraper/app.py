58c4927ceefc2923afe174746a41d982c5c614e5
JXChin02 <jxchin.2021@scis.smu.edu.sg>
2025-04-20 16:33:48 +0800

    [JX] update readme

--- ticketmaster-scraper/app.py
+++ ticketmaster-scraper/app.py
@@ -19,5 +19,5 @@ def ticketmaster_scrape():
     return jsonify({"status": "Scraping started"}), 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 3001))
     app.run(host='0.0.0.0', debug=True, port=port)
